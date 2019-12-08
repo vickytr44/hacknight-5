4e649d9398d650690e4ac7d5a5a780b4ce0134c1
kaushal kishore <kaushalkishore@kaushals-MacBook-Pro.local>
2019-12-08 06:57:04 +0530

    Add devils api.

---
+++ devils-api/src/test/java/com/devils/backend/devilsapi/DevilsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devils.backend.devilsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevilsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
