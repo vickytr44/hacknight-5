b2c1d208999e0719cae2f47af917bbd786fcd967
Srikar Reddy <saisrikar.reddy@314ecorp.com>
2019-12-08 07:45:17 +0530

    ui upload

---
+++ ui/build/precache-manifest.f44d3c6ba83d756caf734295947110bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e1d362d2434e0f43630f9982fede841",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ff12a26b48c1cef3a5cf",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "839c89db988f4f67c6cf",
+    "url": "/static/js/2.acd797e7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.acd797e7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ff12a26b48c1cef3a5cf",
+    "url": "/static/js/main.50631ae2.chunk.js"
+  },
+  {
+    "revision": "1b7e1c9deb18e976c8c3",
+    "url": "/static/js/runtime-main.31eef8cd.js"
+  }
+]);
